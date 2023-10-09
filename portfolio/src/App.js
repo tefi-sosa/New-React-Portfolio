2055235fc77ab5034060e75016060591d2fa1b2b
Estefania Sosa <estefi.monumental@gmail.com>
2023-10-09 16:16:43 -0600

    Title screen template

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,16 +2,23 @@ import './App.css';
 import video from './components/video/DJI_0704-trimmed.MP4'
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Title from './components/Title';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+
       <Footer></Footer>
+      <section id='homepage'>
+      <Title/>        
+      </section>
+
       <div className="hero">
         <video autoPlay loop muted play-inline="true" className="back-video">
           <source src={video} type="video/mp4"/>
         </video>
       </div>
+
 
       <Navbar/>
 
---
+++ portfolio/src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Title() {
+  return (
+    <div id='title'>
+      <p className='intro'>Hi, I'm</p>
+      <h1 className='name'>ESTEFANIA SOSA</h1>
+      <h2>FULL STACK DEVELOPER -</h2>
+    </div>
+  )
+}
