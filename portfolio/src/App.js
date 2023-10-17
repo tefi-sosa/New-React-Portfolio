607a41f1ab8ff85efd4526ede629c643fadfe971
Estefania Sosa <estefi.monumental@gmail.com>
2023-10-17 11:55:01 -0600

    first commit for project section of portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,24 +3,29 @@ import video from './components/video/DJI_0704-trimmed.MP4'
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Title from './components/Title';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="app">
 
       <Footer></Footer>
-      <section id='homepage'>
-      <Title/>        
-      </section>
-
-      <div className="hero">
+      <Navbar/>
+       <div className="hero">
         <video autoPlay loop muted play-inline="true" className="back-video">
           <source src={video} type="video/mp4"/>
         </video>
-      </div>
+      </div>     
+      <section id='homepage'>
+        <Title/>        
+      </section>
+      <section id='projects'>
+        <Projects/>
+      </section>
 
 
-      <Navbar/>
+
+      
 
     </div>
   );
---
+++ portfolio/src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProjectCard from './ProjectCard'
+
+
+export default function Projects() {
+  return (
+    <div>
+      <ProjectCard/>
+    </div>
+  )
+}
