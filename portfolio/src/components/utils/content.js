d29943b22f85298788aae32491b8a060258ab341
Estefania Sosa <estefi.monumental@gmail.com>
2023-10-24 11:52:07 -0600

    Created projects data file and added images

---
+++ portfolio/src/components/utils/content.js
@@ -0,0 +1,25 @@
+import image1 from '../images/ossira-homepage-screenshot-cropped.png'
+import image2 from '../images/ossira-product.png'
+import image3 from '../images/ossira-cart-screenshot.png'
+import image4 from '../images/ossira-myorders-screenshot.png'
+import image5 from '../images/wanderlust.png'
+import image6 from '../images/wanderlust2.png'
+
+export const accordionData = [
+  {
+    title: 'OSSIRA',
+    content: `Developed an eCommerce site that features a modern user interface for efficient search, purchase of products, and management of purchases/wishlist items`,
+    skills: `React | Redux | Hooks | ReactRouter | JWT | CSS | JavaScript | Axios | Express | Sequelize | Node.js | Formik | Bcrypt.js | PostgreSQL | Heroku`,
+    githublink: `https://github.com/tefi-sosa/OSSIRA`,
+    videolink: `https://www.youtube.com/watch?v=o2NQC87WpaU&feature=youtu.be`,
+    images: [image1, image2, image3, image4]
+  },
+  {
+    title: 'WANDERLUST',
+    content: `Developed a hiking application specific to the geographical location of Argentina to facilitate convenience and awareness surrounding hiking trails in the area`,
+    skills: `HTML | CSS | JavaScript | Bootstrap | Axios | Express | Node.js | PostgreSQL | Heroku`,
+    githublink: `https://github.com/tefi-sosa/Foundations-FinalProject`,
+    videolink: `https://youtu.be/WjzjiHF6Uas`,
+    images: [image5, image6]
+  },
+];
