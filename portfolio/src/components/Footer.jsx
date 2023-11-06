d0965df530cd1f67316c5ab5d755cee8de7fe54a
Estefania Sosa <estefi.monumental@gmail.com>
2023-11-06 16:00:27 -0700

    Fixed react class error

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -8,9 +8,9 @@ function Footer() {
         <a href='#homepage'><h4>E S<br/>O .</h4></a>
       </div>
       <div className='contact-icons'>
-        <a href='https://www.linkedin.com/in/estefania-sosa-70b63a1a8/' target="_blank" rel="noreferrer noopener"><i class="fa-brands fa-linkedin fa-l"></i></a>
-        <a href='#contact'><i class="social-icon fa-solid fa-envelope fa-l"></i></a>
-        <a href='https://github.com/tefi-sosa' target="_blank" rel="noreferrer noopener"><i class="fa-brands fa-github fa-l"></i></a>
+        <a href='https://www.linkedin.com/in/estefania-sosa-70b63a1a8/' target="_blank" rel="noreferrer noopener"><i className="fa-brands fa-linkedin fa-l"></i></a>
+        <a href='#contact'><i className="social-icon fa-solid fa-envelope fa-l"></i></a>
+        <a href='https://github.com/tefi-sosa' target="_blank" rel="noreferrer noopener"><i className="fa-brands fa-github fa-l"></i></a>
       </div>
     </section>
   )
