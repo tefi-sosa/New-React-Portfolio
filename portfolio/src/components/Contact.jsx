a9584a17d187bec3990b39ea1ff03343566db35d
Estefania Sosa <estefi.monumental@gmail.com>
2023-11-07 10:53:43 -0700

    Added contact template

---
+++ portfolio/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EmailForm from './EmailForm'
+
+const Contact = () => {
+  return (
+    <div className='half'>
+      <h4>—</h4>
+      <EmailForm/>
+    </div>
+  )
+}
+
+export default Contact
